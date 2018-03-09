3c6a6c38a0738089c66cd5223cb46ea91eb5e87e
scoomdev <soetaert.chris@gmail.com>
2018-03-09 12:10:15 +0100

    feat/workList & Data
    - Add works data in typescript file (./data/works.ts)
    - hydrate & animate workList page
    
    /!\ array.reverse() => to correct !

--- src/app/work-list/work-list.component.ts
+++ src/app/work-list/work-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { works } from '../../data/works';
+
 @Component({
   selector: 'app-work-list',
   templateUrl: './work-list.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkListComponent implements OnInit {
 
-  constructor() { }
+  workList;
+
+  constructor() {
+    this.workList = works.reverse();
+  }
 
   ngOnInit() {
+  }
+
+  cardHoverAnimation(event) {
+    const cardId = event.target.dataset['id'];
+    const cards = document.getElementsByName('card');
+    for (let i = 0; i < cards.length; i++) {
+      const element = cards[i];
+      if (element.dataset['id'] !== cardId) {
+        element.style.opacity = '0.2';
+        element.style.filter = 'blur(3px)';
+      }
+    }
+  }
+
+  cardOutAnimation(event) {
+    const cards = document.getElementsByName('card');
+    for (let i = 0; i < cards.length; i++) {
+      const element = cards[i];
+      element.style.opacity = '1';
+      element.style.filter = 'blur(0)';
+    }
   }
 
 }
---
+++ src/data/works.ts
@@ -0,0 +1,71 @@
+export const works = [
+    {
+        id: 0,
+        title: 'Altissia - P1 OCS',
+        description: 'Premier projet de la formation OpenClassRooms',
+        link: 'http://sccomarts.fr/altissia',
+        skills: [
+            {name: 'wordpress', color: 'blue'},
+            {name: 'PHP', color: 'php'},
+            {name: 'CSS', color: 'css'}
+        ],
+        image: 'altissia-bd.jpg',
+        imageHD: 'altissia-hd.jpg'
+    },
+    {
+        id: 1,
+        title: 'Forteroche - P3 OCS',
+        description: 'Troisième projet de la formation OpenClassRooms',
+        link: 'http://forteroche.scoomdev.eu/',
+        skills: [
+            {name: 'PHP', color: 'php'},
+            {name: 'Silex', color: 'blue'},
+            {name: 'HTML', color: 'html'},
+            {name: 'CSS', color: 'css'}
+        ],
+        image: 'forteroche-bd.jpg',
+        imageHD: 'forteroche-hd.jpg'
+    },
+    {
+        id: 2,
+        title: 'Louvre - P4 OCS',
+        description: 'Quatrième projet de la formation OpenClassRooms',
+        link: 'http://projet-4.alwaysdata.net',
+        skills: [
+            {name: 'PHP', color: 'php'},
+            {name: 'Symfony', color: 'symfony'},
+            {name: 'HTML', color: 'html'},
+            {name: 'CSS', color: 'css'}
+        ],
+        image: 'louvre-bd.jpg',
+        imageHD: 'louvre-hd.jpg'
+    },
+    {
+        id: 3,
+        title: 'NAO - P5 OCS',
+        description: 'Cinquième projet de la formation OpenClassRooms',
+        link: 'https://teamp5.alwaysdata.net',
+        skills: [
+            {name: 'PHP', color: 'php'},
+            {name: 'Symfony', color: 'symfony'},
+            {name: 'HTML', color: 'html'},
+            {name: 'CSS', color: 'css'}
+        ],
+        image: 'nao-bd.jpg',
+        imageHD: 'nao-hd.jpg'
+    },
+    {
+        id: 4,
+        title: 'Meetlight - P6 OCS',
+        description: 'Sixième projet de la formation OpenClassRooms',
+        link: 'http://scoomdev.alwaysdata.net/login',
+        skills: [
+            {name: 'PHP', color: 'php'},
+            {name: 'Symfony', color: 'symfony'},
+            {name: 'HTML', color: 'html'},
+            {name: 'CSS', color: 'css'}
+        ],
+        image: 'meetlight-bd.jpg',
+        imageHD: 'meetlight-hd.jpg'
+    }
+];
